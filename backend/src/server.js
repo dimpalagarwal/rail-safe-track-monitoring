5187aca9b0c3963d8bdb1b3de5a4759fb8c7c1a9
Devanshi Yadav <devanshi056btcse24@igdtuw.ac.in>
2026-01-11 18:23:26 +0530

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,15 +6,17 @@ const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
 const mlRoutes = require("./routes/mlRoutes");
 
-const app = express();          // <-- CREATE APP FIRST
+const app = express();  
 
 app.use(cors());
 app.use(express.json());
 
-connectDB();                    // <-- THEN CONNECT DB
-
+// Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/ml", mlRoutes);
+
+// DB
+connectDB(); // DB error can exist, server still runs
 
 app.listen(5000, () => {
   console.log("Server running on port 5000");
