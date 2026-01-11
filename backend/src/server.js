ba0c802f9aba3c6f0bbbc715bae82a4f7b4015b6
Devanshi Yadav <devanshi056btcse24@igdtuw.ac.in>
2026-01-11 17:53:26 +0530

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,15 +6,13 @@ const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
 const mlRoutes = require("./routes/mlRoutes");
 
-const app = express();
+const app = express();          // <-- CREATE APP FIRST
 
 app.use(cors());
 app.use(express.json());
 
-// connect DB FIRST
-connectDB();
+connectDB();                    // <-- THEN CONNECT DB
 
-// routes
 app.use("/api/auth", authRoutes);
 app.use("/api/ml", mlRoutes);
 
