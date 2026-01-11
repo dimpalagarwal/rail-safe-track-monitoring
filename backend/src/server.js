ba98a4afb47d6ce2b559dfaac433d2a357cdffcd
Devanshi Yadav <devanshi056btcse24@igdtuw.ac.in>
2026-01-11 17:25:31 +0530

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,20 +1,22 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-// const connectDB = require("./config/db");
+const connectDB = require("./config/db");
 
 const authRoutes = require("./routes/authRoutes");
 const mlRoutes = require("./routes/mlRoutes");
 
-const app = express(); 
-
-// connectDB();
+const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+// connect DB FIRST
+connectDB();
+
+// routes
 app.use("/api/auth", authRoutes);
-app.use("/api/ml", mlRoutes); 
+app.use("/api/ml", mlRoutes);
 
 app.listen(5000, () => {
   console.log("Server running on port 5000");
